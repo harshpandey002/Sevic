347644b9f37e6a8cc2092cd4854aa4b68da86243
harshpandey002 <coding.harshp@gmail.com>
2021-10-28 15:44:42 +0530

    Header Setup

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+import styles from "@/styles/Header.module.css";
+
+export default function Header() {
+  return (
+    <div className={styles.container}>
+      <nav>
+        <h1>Navbar</h1>
+      </nav>
+    </div>
+  );
+}
