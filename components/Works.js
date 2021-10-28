9bda40ecd76e4f52edbf5be7b7e59f35ccc5d052
harshpandey002 <coding.harshp@gmail.com>
2021-10-28 23:38:53 +0530

    parallax effect on images

--- components/Works.js
+++ components/Works.js
@@ -1,7 +1,46 @@
+import { useEffect, useState } from "react";
 import styles from "@/styles/Works.module.css";
 import { Parallax } from "react-scroll-parallax";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
 export default function Works() {
+  const [ref, inView] = useInView({
+    threshold: 0.5,
+    triggerOnce: true,
+  });
+  const animation = useAnimation();
+
+  useEffect(() => {
+    if (inView) {
+      animation.start("animate");
+    } else {
+      animation.start("initial");
+    }
+  });
+
+  const test = {
+    initial: {
+      opacity: 0,
+    },
+    animate: {
+      opacity: 1,
+      transition: { staggerChildren: 0.15, staggerDirection: 1 },
+    },
+  };
+
+  const item = {
+    initial: {
+      opacity: 0,
+      y: 80,
+    },
+    animate: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 0.8, type: "ease" },
+    },
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.content}>
@@ -10,24 +49,34 @@ export default function Works() {
             <h1>Our Works</h1>
           </div>
         </Parallax>
-        <div className={styles.boxes}>
+        <motion.div
+          ref={ref}
+          variants={test}
+          initial="initial"
+          animate={animation}
+          className={styles.boxes}
+        >
           <div className={styles.top}>
             <Box
+              variants={item}
               width={194}
               height={334}
               src="https://source.unsplash.com/random/500x401"
             />
             <Box
+              variants={item}
               width={247}
               height={251}
               src="https://source.unsplash.com/random/500x411"
             />
             <Box
+              variants={item}
               width={193}
               height={334}
               src="https://source.unsplash.com/random/500x410"
             />
             <Box
+              variants={item}
               width={248}
               height={251}
               src="https://source.unsplash.com/random/500x421"
@@ -35,36 +84,59 @@ export default function Works() {
           </div>
           <div className={styles.bottom}>
             <Box
+              variants={item}
               width={248}
               height={251}
               src="https://source.unsplash.com/random/520x412"
             />
             <Box
+              variants={item}
               width={192}
               height={335}
               src="https://source.unsplash.com/random/520x402"
             />
             <Box
+              variants={item}
               width={247}
               height={251}
               src="https://source.unsplash.com/random/501x402"
             />
             <Box
+              variants={item}
               width={192}
               height={335}
               src="https://source.unsplash.com/random/502x402"
             />
           </div>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
 }
 
-export const Box = ({ width, height, src }) => {
+export const Box = ({ width, height, src, variants }) => {
+  const [offsetY, setOffsetY] = useState(0);
+  const handleScroll = () => setOffsetY(window.pageYOffset);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  console.log(offsetY);
+
   return (
-    <div style={{ width, height }} className={styles.img}>
-      <img src={src} alt="" />
-    </div>
+    <motion.div
+      variants={variants}
+      style={{ width, height }}
+      className={styles.img}
+    >
+      <img
+        style={{ transform: `translateY(${offsetY * 0.1 - 80}px)` }}
+        src={src}
+        alt=""
+      />
+    </motion.div>
   );
 };
