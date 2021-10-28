15aa8458045dcd31f9261fb8f1cb905595d0e8a1
harshpandey002 <coding.harshp@gmail.com>
2021-10-28 18:44:51 +0530

    Hero Layout and Animation

---
+++ components/Hero.js
@@ -0,0 +1,51 @@
+import styles from "@/styles/Hero.module.css";
+
+export default function Hero() {
+  const heroText =
+    "We create highly responsive applications with the remarkable aesthetics that deliver a spectrum of result oriented digital experience.";
+  let i = -1;
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <div className={styles.top}>
+          <div className={styles.heading}>
+            <div>
+              <h1>
+                <span>UI/UX Design.</span>{" "}
+                <span style={{ color: "#9c9c9c" }}>Website Development.</span>{" "}
+              </h1>
+            </div>
+            <div>
+              <h1>
+                <span style={{ color: "#9c9c9c" }}>Branding.</span>{" "}
+                <span style={{ color: "#727272" }}>Business Cunsulting.</span>{" "}
+              </h1>
+            </div>
+          </div>
+
+          <div className={styles.squareImg}>
+            {/* <img
+              src="https://images.unsplash.com/photo-1634153570366-deda92ecf625?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=500&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTYzNTQyNjExMw&ixlib=rb-1.2.1&q=80&w=500"
+              alt=""
+            /> */}
+          </div>
+        </div>
+        <div className={styles.bottom}>
+          <div className={styles.rectangleImg}>
+            {/* <img
+              src="https://images.unsplash.com/photo-1632810746169-429fce9489fa?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=500&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTYzNTQyNjEwOQ&ixlib=rb-1.2.1&q=80&w=700"
+              alt=""
+            /> */}
+          </div>
+          <p className={styles.text}>
+            {heroText.split(" ").map((text) => (
+              <div key={i++}>
+                <div>{text}</div>
+              </div>
+            ))}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
+import Hero from "@/components/Hero";
 import Layout from "@/components/Layout";
 import styles from "@/styles/Home.module.css";
 
 export default function Home() {
   return (
     <Layout>
-      <h1>Sevic</h1>
+      <Hero />
     </Layout>
   );
 }
