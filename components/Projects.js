0d6b1420539941a4c8e6a6692d33d653e9ac627a
harshpandey002 <coding.harshp@gmail.com>
2021-11-06 19:08:02 +0530

    Trigger Projects on Scroll

--- components/Projects.js
+++ components/Projects.js
@@ -1,14 +1,29 @@
+import { useState } from "react";
 import styles from "@/styles/Projects.module.css";
 import BeyondPinks from "./Projects/BeyondPinks";
 import EduCompanion from "./Projects/EduCompanion";
 import Hysea from "./Projects/Hysea";
+import ScrollTrigger from "react-scroll-trigger";
 
 export default function Projects() {
+  const [project, setProject] = useState("");
+
   return (
     <div className={styles.container}>
-      <EduCompanion />
-      <BeyondPinks />
-      <Hysea />
+      {project == "EduCompanion" && <EduCompanion />}
+      {project == "BeyondPinks" && <BeyondPinks />}
+      {project == "Hysea" && <Hysea />}
+      <div className={styles.flex}>
+        <ScrollTrigger onEnter={() => setProject("EduCompanion")}>
+          <p>EduCompanion</p>
+        </ScrollTrigger>
+        <ScrollTrigger onEnter={() => setProject("BeyondPinks")}>
+          <p>BeyondPinks</p>
+        </ScrollTrigger>
+        <ScrollTrigger onEnter={() => setProject("Hysea")}>
+          <p>Hysea</p>
+        </ScrollTrigger>
+      </div>
     </div>
   );
 }
